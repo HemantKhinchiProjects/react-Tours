fe5836d4cf2ae5a81f56acc9537e7c6aa17a179d
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-17 15:18:49 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Loading from './Loading';
 import Tours from './Tours';
 import './style.css';
 const url = 'https://course-api.com/react-tours-project';
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [tour, setTour] = useState([]);
+  if (Loading) {
+    return <Loading />;
+  }
   return (
     <div>
-      <Loading />
       <h2>Tours Project Setup</h2>
     </div>
   );
