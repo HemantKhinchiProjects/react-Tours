1413a6c8c87a43d4c27e54dc116f2858bd0cb7c5
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-17 11:27:39 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import Loading from './Loading';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <Loading />
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
